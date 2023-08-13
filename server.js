eec91e541ef8a82dd1307da35b191154569203eb
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-08-13 11:09:40 +0530

    fetch products and fetch by id created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 const app = express();
+
+app.use(express.json()); //middleware
 
 //router
 
@@ -8,9 +11,45 @@ app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.listen(3000, () => {
-  console.log("API running on port 3000");
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  // console.log(req.body);
+  // res.send(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// app.listen(3000, () => {
+//   console.log("API running on port 3000");
+// });
+
+//update a product
+// app.put("products:id", asyn); //can use put or patch method
+
+mongoose.set("strictQuery", false); //rather than schema nothing can be sent
 
 mongoose
   .connect(
@@ -18,7 +57,9 @@ mongoose
   )
   .then(() => {
     console.log("connected to DB");
-    app.listen;
+    app.listen(3000, () => {
+      console.log("API running on port 3000");
+    });
   })
   .catch((error) => {
     console.log(error);
