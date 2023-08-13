0f4982e822d9cafcb970aaae578ca9dfeafa732b
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-08-13 14:07:16 +0530

    delete working

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
-app.post("/products", async (req, res) => {
+app.post("/product", async (req, res) => {
   // console.log(req.body);
   // res.send(req.body);
   try {
@@ -65,6 +65,20 @@ app.put("/products/:id", async (req, res) => {
 });
 
 //delete a product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 mongoose.set("strictQuery", false); //rather than schema nothing can be sent
 
